f1c48c7cdac64b33a6be029aed138c5f8171491a
Louis Nguyen <louis.nguyen550@gmail.com>
2025-09-28 10:00:09 -0400

    implement navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,27 @@
-import React, { ReactNode } from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { ReactNode } from 'react';
 
-const RootLayout = ({children}: {children: ReactNode}) => {
-  return (
-    <div>RootLayout</div>
-  )
-}
+const RootLayout = ({ children }: { children: ReactNode }) => {
+	return (
+		<div className="root-layout">
+			<nav>
+				<Link
+					href="/"
+					className="flex items-center gap-2"
+				>
+					<Image
+						src="/logo.svg"
+						alt="logo"
+						width={38}
+					/>
+					<h2 className="text-primary-100">Prepmax</h2>
+				</Link>
+			</nav>
 
-export default RootLayout
+			{children}
+		</div>
+	);
+};
+
+export default RootLayout;
