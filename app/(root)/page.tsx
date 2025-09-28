5c40f73568255ed874be78b44582ee5e7cba9127
Louis Nguyen <louis.nguyen550@gmail.com>
2025-09-28 10:25:01 -0400

    call interview props

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
+import InterviewCard from '@/components/InterviewCard';
 import { Button } from '@/components/ui/button';
+import { dummyInterviews } from '@/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
@@ -32,7 +34,14 @@ const page = () => {
 			<section className="flex flex-col gap-6 mt-8">
 				<h2>Your interviews</h2>
 				<div className="interviews-section">
-					<p>You haven&apos;t taken any interviews yet</p>
+					{dummyInterviews.map((interview) => (
+						<InterviewCard
+							{...interview}
+							key={interview.id}
+						/>
+					))}
+
+					{/* <p>You haven&apos;t taken any interviews yet</p> */}
 				</div>
 			</section>
 
@@ -40,7 +49,13 @@ const page = () => {
 				<h2>Take an Interview</h2>
 
 				<div className="interviews-section">
-					<p>There are no interviews available</p>
+					{dummyInterviews.map((interview) => (
+						<InterviewCard
+							{...interview}
+							key={interview.id}
+						/>
+					))}{' '}
+					{/* <p>There are no interviews available</p> */}
 				</div>
 			</section>
 		</>
