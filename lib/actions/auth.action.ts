bf91efeaca8734847b40566ee92815fb2d5267ac
Louis Nguyen <louis.nguyen550@gmail.com>
2025-10-05 13:09:08 -0400

    implement get current user function

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -91,3 +91,35 @@ export async function setSessionCookie(idToken: string) {
 		sameSite: 'lax',
 	});
 }
+
+// this function gets the current user and sees if they are logged in to access the home page
+export async function getCurrentUser(): Promise<User | null> {
+	const cookieStore = await cookies();
+
+	const sessionCookie = cookieStore.get('session')?.value;
+
+	if (!sessionCookie) {
+		return null;
+	}
+
+	try {
+		const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+		const userRecord = await db
+			.collection('users')
+			.doc(decodedClaims.uid)
+			.get();
+
+		if (!userRecord.exists) {
+			return null;
+		}
+
+		return {
+			...userRecord.data(),
+			id: userRecord.id,
+		} as User;
+	} catch (error) {
+		console.log(error);
+
+		return null;
+	}
+}
