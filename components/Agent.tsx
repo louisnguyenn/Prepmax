ffc32efb786274d606f3d2d4174fe4fa41ec88fe
Louis Nguyen <louis.nguyen550@gmail.com>
2025-10-06 21:29:53 -0400

    create ai and user interface

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,16 +1,45 @@
-import Image from "next/image"
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
-const Agent = () => {
-  return (
-    <div className="call-view">
-      <div className="card-interviewer">
-        <div className="avatar">
-          <Image src='/ai-avatar.png' alt="vapi" width={65} height={54} className="object-cover" />
-        </div>
+const Agent = ({ userName }: AgentProps) => {
+	const isSpeaking = true;
+
+	return (
+		<>
+			<div className="call-view">
+				<div className="card-interviewer">
+					<div className="avatar">
+						<Image
+							src="/ai-avatar.png"
+							alt="vapi"
+							width={65}
+							height={54}
+							className="object-cover"
+						/>
+						{isSpeaking && <span className="animate-speak" />}
+					</div>
+					<h3>AI Interviwer</h3>
+				</div>
+
+				<div className="card-border">
+					<div className="card-content">
+						<Image
+							src="/user-avatar.png"
+							alt="user avatar"
+							width={540}
+							height={540}
+							className="rounded-full object-cover size-[120px]"
+						/>
+						<h3>{userName}</h3>
+					</div>
+				</div>
+			</div>
+
+      <div className="w-full flex justify-center">
+        {}
       </div>
-    </div>
-  )
-}
+		</>
+	);
+};
 
-export default Agent
+export default Agent;
